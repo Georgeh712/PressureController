60edc6e492aaa143fbe5b459b8e49ecc9a6227f1
Georgeh712 <86979254+Georgeh712@users.noreply.github.com>
2022-07-12 12:24:37 +0100

    Removed self prefixes where not needed in Logs.py

--- Logs.py
+++ Logs.py
@@ -4,13 +4,13 @@ import random
 class Log:
     def __init__(self, fileName):
         self.fileName = fileName
-        self.fileNameDate = self.fileName[0:10]
+        fileNameDate = self.fileName[0:10]
         hour = self.fileName[11:13]
         mins = self.fileName[14:16]
-        print(hour + mins)
-        self.fileNameForm = "Logs/" + self.fileNameDate + "-" + hour + mins + ".txt"
 
-        self.f = open(self.fileNameForm, 'x')
+        fileNameForm = "Logs/" + fileNameDate + "-" + hour + mins + ".txt"
+
+        self.f = open(fileNameForm, 'x')
         startString = "Log file started: " + self.fileName
         self.f.write(startString + "\n")
 
