ef376428c11fd5d822b73461bc1c79e5234a18fb
Gillingham <f87580@heraeus.com>
2022-10-21 11:25:36 +0100

    Morgan update for Due board

--- potentiometer.ino/potentiometer.ino.ino
+++ potentiometer.ino/potentiometer.ino.ino
@@ -1,19 +1,19 @@
 // potentiometer.ino
 // reads a potentiometer sensor and sends the reading over serial
 
-int mfcPin = A0; // the potentiometer is connected to analog pin 0
-int pressurePin = A1;
-int outputPin = 5;
+int mfcPin = A1; // the potentiometer is connected to analog pin 0
+int pressurePin = A2;
+int outputPin = 3;
 int sensorValue; // an integer variable to store the potentiometer reading
 
 void setup() { // this function runs once when the sketch starts up
   // initialize serial communication :
-  Serial.begin(250000);
-  Serial.setTimeout(250);
+  Serial.begin(9600);
+  //Serial.setTimeout(250);
 }
 
 void loop() { // this loop runs repeatedly after setup() finishes
-  analogReadResolution(16);
+  analogReadResolution(12);
   sensorValue = analogRead(pressurePin); // read the sensor
   Serial.println(sensorValue); // output reading to the serial line
   sensorValue = analogRead(mfcPin);
